df1a2c48f7c49b14d6dc9801f2c8b54136a1d37b
Ferlgrand <efe.yukselen@ozu.edu.tr>
2019-07-02 17:20:21 +0300

    Basic json reading for test

--- src/FileReader.py
+++ src/FileReader.py
@@ -1,10 +1,13 @@
 import json
+from pathlib import Path
 
 # Working code with the assumption that the input has "UTF-8" encoding
 
 
 def test():
 
+    root = r"C:\Users\efe.yukselen\PycharmProjects\datainjection\data\requests"
+    # Use pathlib with this root
     fileName = r"C:\Users\efe.yukselen\PycharmProjects\datainjection\data\requests\2015\09\30\zed-log\07-requests.json"
     testFile = open(fileName, "r")
     contents = testFile.read()
