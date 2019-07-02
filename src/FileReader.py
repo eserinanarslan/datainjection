2ec415425bc2bf01897150f0965f47459a87b14b
Ferlgrand <efe.yukselen@ozu.edu.tr>
2019-07-02 15:53:10 +0300

    Basic json reading for test

--- src/FileReader.py
+++ src/FileReader.py
@@ -1,6 +1,8 @@
 import json
 
 # Working code with the assumption that the input has "UTF-8" encoding
+
+
 def test():
 
     fileName = r"C:\Users\efe.yukselen\PycharmProjects\datainjection\data\requests\2015\09\30\zed-log\07-requests.json"
